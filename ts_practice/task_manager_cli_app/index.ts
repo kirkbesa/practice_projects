4192496061da30e9934a59dd3e03a6d4bc8d558a
Kirk Besa <kirkpatrickbesa@gmail.com>
2025-04-22 23:45:31 +0800

    Feat: TaskManager CLI: Add User

--- ts_practice/task_manager_cli_app/index.ts
+++ ts_practice/task_manager_cli_app/index.ts
@@ -9,6 +9,71 @@ function askQuestion(question: string): Promise<string> {
     return new Promise(answer => rl.question(question, answer))
 }
 
+interface User {
+    name: string
+    role: string
+}
+
+enum UserRole {
+    ADMIN = 'admin',
+    USER = 'user',
+    MANAGER = 'manager',
+}
+
+let users: User[] = []
+
+async function addUser() {
+    type Step = 'askName' | 'askRole' | 'done'
+    let step: Step = 'askName'
+    let newUser: User = { name: '', role: '' }
+
+    while (step !== 'done') {
+        switch (step) {
+            case 'askName':
+                const name: string = await askQuestion('Enter name: ')
+
+                if (!name) {
+                    console.log('Name cannot be empty.')
+                    continue
+                } else if (name[0] === ' ') {
+                    console.log('Name cannot start with a space.')
+                    continue
+                } else if (!isNaN(Number(name[0]))) {
+                    console.log('Name cannot start with a number.')
+                    continue
+                } else {
+                    const existingUser = users.find(user => user.name === name)
+                    if (existingUser) {
+                        console.log('User already exists.')
+                        continue
+                    }
+                }
+
+                newUser.name = name
+                step = 'askRole'
+                break
+
+            case 'askRole':
+                const role: string = await askQuestion('Enter role (admin/user/manager): ')
+
+                if (!role) {
+                    console.log('Role cannot be empty.')
+                    continue
+                } else if (!Object.values(UserRole).includes(role as UserRole)) {
+                    console.log('Invalid role. Choose from admin, user, or manager.')
+                    continue
+                }
+
+                newUser.role = role
+                step = 'done'
+                break
+        }
+    }
+
+    users.push(newUser)
+    console.log('User added successfully!')
+}
+
 async function startApp() {
     console.log('Weclome to TaskManager CLI!')
 
@@ -17,7 +82,12 @@ async function startApp() {
     while (running) {
         const input = await askQuestion('Type a command: ')
         switch (input.toLowerCase()) {
+            case 'add user':
+                console.clear()
+                await addUser()
+                break
             case 'help':
+                console.clear()
                 console.log('List of commands:')
                 break
             case 'exit':
