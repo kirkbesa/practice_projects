943d692062684ef7f814082b299eeb8630e4a51f
Kirk Besa <kirkpatrickbesa@gmail.com>
2025-04-23 19:55:21 +0800

    chore(taskmanagercli): remove unused validators.ts file

--- task_manager_cli_app/utils/validators.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function isIdExisting(input, list) {
-    const existingElement = list.findIndex(element => element.id === Number(input));
-    if (existingElement === -1) {
-        return { valid: false, message: 'Invalid ID. ID not found' };
-    }
-    return { valid: true, message: '' };
-}
--- task_manager_cli_app/utils/validators.ts
+++
@@ -1,9 +0,0 @@
-type Result = { valid: boolean; message: string }
-
-function isIdExisting(input: string, list: any[]): Result {
-    const existingElement = list.findIndex(element => element.id === Number(input))
-    if (existingElement === -1) {
-        return { valid: false, message: 'Invalid ID. ID not found' }
-    }
-    return { valid: true, message: '' }
-}
